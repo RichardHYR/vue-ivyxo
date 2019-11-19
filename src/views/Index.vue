b737c21e1c8175af63cd5fc86a71f59d6c9d3a32
RichardHYR <richard_hyr@163.com>
2019-11-20 00:21:04 +0800

    添加注册和登录按钮UI

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,21 @@
 <template>
   <div class>
-    <Header></Header>
-    我是主页
+
+    <el-container>
+
+      <el-header>
+        <div class="reg">
+          <el-button type="primary" round>注册</el-button>
+          <el-button type="success" round>登录</el-button>
+        </div>
+      </el-header>
+
+      <el-main>Main</el-main>
+
+    </el-container>
+
   </div>
 </template>
-
 <script>
 export default {
   name: "Index",
@@ -16,11 +27,30 @@ export default {
   methods: {
     
   },
-  components: {},
+  components: {
+
+  },
   mounted() {}
 };
 </script>
 
 <style scoped src=''>
 
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .reg{
+    margin-left: 80%;
+  }
 </style>
