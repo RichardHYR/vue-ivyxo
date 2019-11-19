1c6ddbdc8a570f99e7fce58fa9edeff5323239b5
RichardHYR <richard_hyr@163.com>
2019-11-19 22:11:10 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+// //快速点击插件
+// import fastClick from "fastClick";
+//图片懒加载
+import VueLazyLoad from 'vue-lazyload'
+import api from './utils/api'
+import component from './components'
+
+Vue.use(VueLazyLoad)
+Vue.use(api)
+Vue.use(component)
+Vue.use(ElementUI)
+// 设置为 false 以阻止 vue 在启动时生成生产提示。
+Vue.config.productionTip = false
+// // 加载FastClick
+// fastClick.attach(document.body)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+}).$mount('#app')
