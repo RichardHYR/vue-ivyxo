43f6635672b61bca8206ff5bc01672a31e06b0f5
RichardHYR <richard_hyr@163.com>
2019-12-31 00:15:52 +0800

    添加了打包之后去掉console的插件

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     assetsDir: "assets",
     devServer: {
         // host: "localhost",
-        port: 8080, // 端口号
+        port: 9999, // 端口号
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
